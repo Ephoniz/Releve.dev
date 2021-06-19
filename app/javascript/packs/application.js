8e07c0330822a9438d22508f45e7e5fe1d325200
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-19 16:13:04 +0200

    dashborad base style done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity(elem, {
+    // options
+  freeScroll: true,
+  wrapAround: true
+
+  });
 });
