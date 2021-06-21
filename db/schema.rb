103c50c977f7160a6e495956ce978b720165d620
Marta Sintes <marta_sintes@hotmail.com>
2021-06-21 10:05:30 +0200

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_131315) do
+ActiveRecord::Schema.define(version: 2021_06_20_153135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_131315) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "language_id"
     t.string "image_url"
+    t.string "level"
     t.index ["language_id"], name: "index_courses_on_language_id"
   end
 
